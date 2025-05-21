75bd8e7c431fe9f4ff8fac8fcae1d0458975b7a2
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-05-21 21:00:05 +0500

    changed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello World' });
+  res.json({ message: 'Hello World 2' });
 });
 
 app.listen(PORT, () => {
